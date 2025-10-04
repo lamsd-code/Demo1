31294c155523d6598fe79a9ea05163dd746adb66
Lâm Mai <lamssdd910@gmail.com>
2025-10-04 08:27:43 +0700

    up

---
+++ src/main/java/vn/iotstar/bai11/Bai11Application.java
@@ -0,0 +1,13 @@
+package vn.iotstar.bai11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bai11Application.class, args);
+	}
+
+}
---
+++ src/test/java/vn/iotstar/bai11/Bai11ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.iotstar.bai11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bai11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
